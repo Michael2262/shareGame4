2ce9cf454b23ee7358cf56cafe86d08b546df61c
DESKTOP-HH8P8VJ\陳政傑 <sa0921m3@gmail.com>
2023-12-06 01:16:07 +0800

    1.新增移動動畫相關 2.調整地圖配置

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        SetAnimation();
+    }
+
+    public void SetAnimation() 
+    {
+        //Mathf.Abs，unity提供的數學方法，取絕對值
+        anim.SetFloat("velocityX",Mathf.Abs(rb.velocity.x));
+    }
+
+}
