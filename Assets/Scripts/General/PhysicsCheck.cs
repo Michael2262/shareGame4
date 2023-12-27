206474ef96da801d97090b4b9f5c8089bf9eae84
DESKTOP-HH8P8VJ\陳政傑 <sa0921m3@gmail.com>
2023-12-28 01:47:49 +0800

    1.調整物理判斷牆面

--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    private CapsuleCollider2D coll;
+
     [Header("檢測參數")]
+    public bool manual;
     public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
     public float checkRaduis;
 
     public LayerMask groundLayer;    
     
     [Header("狀態")]
     public bool isGround;
-    
+    public bool touchLeftWall;
+    public bool touchRightWall;
+
+
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+
+        if (!manual) 
+        {
+            //包圍框（Bounds）
+            rightOffset = new Vector2((coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+            //直接使用上一行的值
+            leftOffset = new Vector2(-rightOffset.x, rightOffset.y);
+            
+        }
+
+    }
+
     private void Update()
     {
         check();
@@ -21,12 +44,19 @@ public class PhysicsCheck : MonoBehaviour
     void check() 
     {
         //檢測地面
-        isGround = Physics2D.OverlapCircle((Vector2)transform.position, checkRaduis, groundLayer);
+        isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRaduis, groundLayer);
+
+        //牆體判斷
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRaduis, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRaduis, groundLayer);
+
     }
 
     //Gizmo輔助線
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRaduis);
     }
 }
