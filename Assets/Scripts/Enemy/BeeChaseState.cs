7dac5fb8a57f064aa552a270afe835d512d2a671
DESKTOP-HH8P8VJ\陳政傑 <sa0921m3@gmail.com>
2024-01-21 20:33:42 +0800

    1.繼續完成蜜蜂(缺死亡)

--- Assets/Scripts/Enemy/BeeChaseState.cs
+++ Assets/Scripts/Enemy/BeeChaseState.cs
@@ -8,11 +8,17 @@ public class BeeChaseState : BaseState
     private Attack attack;
     private Vector3 target;
     private Vector3 moveDir;
+    private bool isAttack;
+    private float attackRateCounter =0;
 
     public override void OnEnter(Enemy enemy)
     {
         currentEnemy = enemy;
+        
         currentEnemy.currentSpeed = currentEnemy.chaseSpeed;
+        attack = enemy.GetComponent<Attack>();
+        
+        currentEnemy.lostTimeCounter = currentEnemy.lostTime;
     }
     public override void LogicUpdate()
     {
@@ -20,16 +26,48 @@ public class BeeChaseState : BaseState
             currentEnemy.SwitchState(NPCState.Patrol);
         target = new Vector3(currentEnemy.attacker.position.x, currentEnemy.attacker.position.y + 1.5f, 0);
 
+
+        //§PÂ_§ðÀ»¶ZÂ÷
+        if (Mathf.Abs(target.x - currentEnemy.transform.position.x) <= attack.attackRange && Mathf.Abs(target.y - currentEnemy.transform.position.y) <= attack.attackRange)
+        {
+            {
+                //§ðÀ»
+                isAttack = true;
+                if(!currentEnemy.isHurt)
+                    currentEnemy.rb.velocity = Vector2.zero;
+
+                //­p®É¾¹
+                attackRateCounter -= Time.deltaTime;
+                if (attackRateCounter <= 0)
+                {
+                    currentEnemy.anim.SetTrigger("attack");
+                    attackRateCounter = attack.attackRate;
+                }
+            }
+        }
+        else { isAttack = false; }
+
+        moveDir = (target - currentEnemy.transform.position).normalized;
+
+
+        if (moveDir.x > 0)
+            currentEnemy.transform.localScale = new Vector3(-1, 1, 1);
+        if (moveDir.x < 0)
+            currentEnemy.transform.localScale = new Vector3(1, 1, 1);
+
     }
     public override void PhysicsUpdate()
     {
-        
+        if (!currentEnemy.isHurt && !currentEnemy.wait && !isAttack)
+        {
+            currentEnemy.rb.velocity = moveDir * currentEnemy.currentSpeed * Time.deltaTime;
+        }
     }
 
 
     public override void OnExit()
     {
-        
+        //Debug.Log("EXIT");
     }
 
 }
